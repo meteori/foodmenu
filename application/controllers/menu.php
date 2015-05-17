cf132ed189e700ea24cdadce8332fe5e908a38e3
vimji <jxl0324@gmail.com>
2015-05-17 21:16:33 +0800

    add alert function

--- application/controllers/menu.php
+++ application/controllers/menu.php
@@ -35,9 +35,11 @@ class Menu extends CI_Controller {
       $result = $this->menus->insert($menu);
 
       if ($result) {
-        echo "ok";
+        //echo "ok";
+        $this->alert("ok");
       } else {
-        echo "not ok";
+        $this->alert("not ok");
+        //echo "not ok";
       }
     }
 
@@ -46,6 +48,52 @@ class Menu extends CI_Controller {
 
     $this->load->view('menu/create', $this->data);
   }
+
+ //函数:通用提示
+//参数:提示訊息,類型或網址,窗口名或函數名,延时毫秒Alert("","function","close2",300);
+Function alert($Str,$Typ="back",$TopWindow="",$Tim=100){
+    Echo "<script>".Chr(10);
+    If(!Empty($Str)){
+        Echo "alert(\"Warning:\\n\\n{$Str}\\n\\n\");".Chr(10);
+    }
+ 
+    Echo "function _r_r_(){";
+    $WinName=(!Empty($TopWindow))?"top":"self";
+    Switch (StrToLower($Typ)){
+    Case "#":
+        Break;
+    Case "back":
+        Echo $WinName.".history.go(-1);".Chr(10);
+        Break;
+    Case "reload":
+        Echo $WinName.".window.location.reload();".Chr(10);
+        Break;
+    Case "close":
+        Echo "window.opener=null;window.close();".Chr(10);
+        Break;
+    Case "function":
+        Echo "var _T=new Function('return {$TopWindow}')();_T();".Chr(10);
+        Break;
+        //Die();
+    Default:
+        If($Typ!=""){
+            //Echo "window.{$WinName}.location.href='{$Typ}';";
+            Echo "window.{$WinName}.location=('{$Typ}');";
+        }
+    }
+ 
+    Echo "}".Chr(10);
+ 
+    //為防止Firefox不執行setTimeout
+    Echo "if(setTimeout(\"_r_r_()\",".$Tim.")==2){_r_r_();}";
+    IF($Tim==100){
+        Echo "_r_r_();".Chr(10);
+    }Else{
+        Echo "setTimeout(\"_r_r_()\",".$Tim.");".Chr(10);
+    }
+    Echo "</script>".Chr(10);
+    Exit();
+} 
 }
 
 /* End of file home.php */
